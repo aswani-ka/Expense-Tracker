c541fd8a4ea989ef2174907cd7ccd576fde8ebcd
ASWANI K A <aswanika05@gmail.com>
2025-08-17 15:34:36 +0530

    feat: implement event listener to delete button to remove items from the list

--- script.js
+++ script.js
@@ -51,4 +51,13 @@ document.addEventListener("DOMContentLoaded", () => {
     totalAmount = calculateTotal();
     totalAmountDisplay.textContent = totalAmount.toFixed(2);
   }
+
+  expenseList.addEventListener("click", (e) => {
+    if(e.target.tagName === 'BUTTON') {
+      const expenseId = parseInt(e.target.getAttribute("data-id"));
+      expenses = expenses.filter((expense) => expense.id !== expenseId);
+      renderExpenses();
+      updateTotal();
+    }
+  });
 });
